02c7ec24185521bdba98f3b1a17d014d44e5cbb6
Robert <4060573+rarDevelopment@users.noreply.github.com>
2022-02-13 17:25:50 -0500

    v1.7.2 - refactored socket listener into its own file

--- bot.js
+++ bot.js
@@ -1,14 +1,7 @@
 const Chariot = require("chariot.js");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const GameUpdater = require('./api/GameUpdater.js');
-const LeagueUpdater = require('./api/LeagueUpdater.js');
-const ScoreUpdater = require('./api/ScoreUpdater.js');
-const ConfigDataLayer = require('./api/ConfigDataLayer.js');
-const { createServer } = require('http');
-const ws = require('ws');
-const resources = require('./settings/resources.json');
-const CheckTypes = require('./api/CheckTypes.js');
+const SocketMessageListener = require('./jobs/SocketMessageListener.js');
 
 class FantasyCriticInterim extends Chariot.Client {
     constructor() {
@@ -40,50 +33,7 @@ class FantasyCriticInterim extends Chariot.Client {
 
         this.editStatus('online', { name: 'fc.help', type: 0 });
 
-        const server = createServer();
-        server.listen(resources.botPort);
-
-        const wsServer = new ws.Server({
-            server
-        });
-        let connections = [];
-
-        const guildsToProcess = this.guilds;
-
-        wsServer.on('connection', (socket) => {
-            connections.push(socket);
-            socket.on('message', async (msg) => {
-                try {
-                    const payload = JSON.parse(msg);
-                    console.log(payload);
-                    if (payload.checkToDo) {
-                        const leagueChannels = await ConfigDataLayer.getLeagueChannels();
-                        switch (payload.checkToDo) {
-                            case CheckTypes.GAME_UPDATER_CHECK:
-                                await GameUpdater.sendGameUpdatesToLeagueChannels(guildsToProcess, leagueChannels);
-                                break;
-                            case CheckTypes.SCORE_UPDATER_CHECK:
-                                await ScoreUpdater.sendPublisherScoreUpdatesToLeagueChannels(guildsToProcess, leagueChannels);
-                                break;
-                            case CheckTypes.LEAGUE_ACTION_CHECK:
-                                await LeagueUpdater.sendLeagueUpdatesToLeagueChannels(guildsToProcess, leagueChannels);
-                                break;
-                            default:
-                                console.log("Received invalid check option", payload.checkToDo);
-                        }
-                    }
-                    else {
-                        console.log("Received invalid payload", payload);
-                    }
-                }
-                catch (error) {
-                    console.log("Error handling message on WebSocket", error);
-                }
-            });
-            socket.on('close', () => {
-                connections = connections.filter(s => s !== socket);
-            })
-        });
+        SocketMessageListener.listenOnSocket(this.guilds);
     }
 }
 
